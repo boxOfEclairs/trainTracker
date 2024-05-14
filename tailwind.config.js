19eaa2ec55aeb1076c12fdfc0b8311da4fd0fbef
Claire <johnsonclaire2004@gmail.com>
2024-05-13 22:37:52 -0400

    messing around with tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'alertGreen' : '#0cca4a',
+        'alertRed' : '#ae2a36',
+        'white' : '#F7FBFA',
+        'back' : '#15171A',
+        'front' : '#23232F',
+      },
+      fontFamily: {
+        fira: ['"FiraCode"', "sans-serif"],
+        tommy: ['"MadeTommy"', "sans-serif"],
+      }
+    },
+    fontSize: {
+        'title': '1.5625rem',
+        'subtext' : '0.625rem',
+    }, 
   },
   plugins: [],
 }
+
 
